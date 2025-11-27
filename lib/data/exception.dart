413c5c00c2d50288312d2a4c9995782e25f3ef9b
Muhammad Ismail <ismailcoder366@gmail.com>
2025-11-27 15:05:25 +0500

    exception and api response added

---
+++ lib/data/exception.dart
@@ -0,0 +1,28 @@
+class AppException implements Exception{
+
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  String toString(){
+    return '$_prefix$_message';
+  }
+}
+
+
+class FetchDataException extends AppException{
+  FetchDataException([String? message]) : super(message, 'Error during Communication');
+}
+
+
+class BadRequestException extends AppException{
+  BadRequestException([String? message]) : super(message, 'Invalid Request');
+}
+
+class UnathorizedException extends AppException{
+  UnathorizedException([String? message]) : super(message, 'Unathorized Request');
+}
+class InvalidInputException extends AppException{
+  InvalidInputException([String? message]) : super(message, 'Invalid Input');
+}
