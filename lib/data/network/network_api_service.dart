122bb2eb744857f61ca0f8e21999414ecd2a102a
Muhammad Ismail <ismailcoder366@gmail.com>
2025-11-28 07:55:22 +0500

    base_api and newtwork_api services added

---
+++ lib/data/network/network_api_service.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:mvvm/data/exception.dart';
+import 'package:mvvm/data/network/base_api_service.dart';
+
+class NetworkApiServices extends BaseApiServices{
+  @override
+  Future getGetApiResponse(String url) async{
+
+      dynamic responseJson;
+    try{
+
+      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+
+    }on SocketException{
+      throw FetchDataException('No Internet Connection');
+    }
+      return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async{
+    dynamic responseJson;
+    try{
+
+      http.Response response = await http.post(Uri.parse(url),
+      body: data
+      ).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    }on SocketException{
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+
+  dynamic returnResponse(http.Response response){
+    switch(response.statusCode){
+      case 200 :
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400 :
+        return BadRequestException(response.body.toString());
+      case 404 :
+        return UnathorizedException(response.body.toString());
+        default:
+          throw FetchDataException('Error occured while communicating with server'+'with status code' + response.statusCode.toString());
+    }
+  }
+
+}
